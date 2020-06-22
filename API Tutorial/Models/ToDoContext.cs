6b6ca80659666ea5624a376423b93f0dd8d310dc
Aleks Janev <ajanev@online.gibz.ch>
2020-06-22 12:24:28 +0200

    Projektdateien hinzufügen.

---
+++ API Tutorial/Models/ToDoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_Tutorial.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
